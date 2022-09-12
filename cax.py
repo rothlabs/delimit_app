e46c60fa21fb4b6f5dbf5c46fcc60fc6384cdebf
jre0 <julian@rothlabs.com>
2022-09-11 21:08:56 -0400

    loads insole sketch

--- cax.py
+++ cax.py
@@ -1,2 +1,20 @@
 import config
 
+def FreeCAD_Document(name):
+    return FreeCAD.openDocument(config.cax_path + name +'.FCStd')
+
+doc = FreeCAD_Document('insole')
+
+f = doc.addObject('Part::Extrusion','Extrude')
+f.Base = doc.getObjectsByLabel('sketch')[0]
+f.DirMode = "Normal"
+f.DirLink = None
+f.LengthFwd = 10.0
+f.LengthRev = 0.0
+f.Solid = True
+f.Reversed = False
+f.Symmetric = False
+f.TaperAngle = 0.0
+f.TaperAngleRev = 0.0
+
+doc.recompute()
