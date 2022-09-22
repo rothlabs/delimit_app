06454c394f8b868f0db0440261fbf7bea5b47817
jre0 <julian@rothlabs.com>
2022-09-22 13:23:06 -0400

    update freecad example and core
    removed uneeded imports and added vect def

--- cax/freecad/worker.py
+++ cax/freecad/worker.py
@@ -6,8 +6,8 @@ import core
 
 shoe = core.Template('shoe')
 
-insole_sketch_yz = shoe.select('insole/common/extrude_yz/sketch_yz')
+sketch = shoe.select('insole/common/extrude_yz/sketch_yz')
 
-# insert svg geometry into insole_sketch_yz
+# insert svg geometry into sketch
 shoe.doc.recompute()
 
