32c70ada9f94ad2bde4d96e0029136c838a7f6f9
Julian Erickson <julian@rothlabs.com>
2023-08-05 20:37:33 +0000

    save before vase3

--- web/core/static/core/store/reckon/vase.js
+++ web/core/static/core/store/reckon/vase.js
@@ -1,6 +1,7 @@
-import {Vector3, Vector4, MathUtils, CatmullRomCurve3, Box3} from 'three';
-import {NURBSCurve} from 'three/examples/jsm/curves/NURBSCurve';
 import {current} from 'immer';
+import {Vector3, Matrix4, MathUtils, CatmullRomCurve3, Box3, Raycaster, Mesh, MeshBasicMaterial} from 'three';
+//import {NURBSCurve} from 'three/examples/jsm/curves/NURBSCurve';
+import {ParametricGeometry} from 'three/examples/jsm/geometries/ParametricGeometry';
 
 const v1 = new Vector3();
 const v2 = new Vector3();
@@ -17,8 +18,19 @@ for(let v=0; v<1/res_v; v++){
     }
 }
 
+const origin = new Vector3();
+const direction = new Vector3();
+const raycaster = new Raycaster();
+const material = new MeshBasicMaterial();
+
+const geo_res = 60;
+const rot_res = 60;
+const rotation = new Matrix4().makeRotationY(Math.PI*2/rot_res);
+
+const layer_height = 6;
+
 export const vase = {
-    layer_height: 4,
+    
     code_res: .2, // code_res = arc length between G1 if constant curve like an arc
     node(d, n){
         try{
@@ -31,33 +43,37 @@ export const vase = {
             surface.get_point(1, 0, v1);
             const start_y = v1.y;
             const bb1 = new Box3();
-            for(let v=0; v<1/res_v; v++){
+            for(let v=0; v<1/res_v; v++){ 
                 //u_idx[v] = 0;
                 for(let u=0; u<1/res_u; u++){
                     surface.get_point(1-u*res_u, v*res_v, tests[v][u]);
                     bb1.expandByPoint(tests[v][u]);
                 }
             }
-            var layer_count = 0;
-            var ty = start_y;
+
+
+            const geo_obj = new Mesh(
+                new ParametricGeometry(surface.get_point, geo_res, geo_res),
+                material
+            );
+            var y = start_y;
+            origin.set(0,y,1000);
+            direction.set(0,y,0).sub(origin);
             for(let i=0; i<10000; i++){
-                for(let v=0; v<1/res_v; v++){
-                    ty = start_y - (layer_count * this.layer_height + this.layer_height * v*res_v);
-                    //var v2 = tests[v].slice(u_idx[v], u_idx[v]+50).sort((a,b)=> Math.abs(a.y-ty)-Math.abs(b.y-ty))[0]; // Don't need to sort this ?!?!?!?!?! instead, just search down u from last time at this v
-                    //var v2 = tests[v][u_idx[v]];
-                    var pt = tests[v].sort((a,b)=> Math.abs(a.y-ty)-Math.abs(b.y-ty))[0];
-                    //if(Math.abs(pt.y-ty) < this.layer_height/2){
-                        pts.push(pt.clone());
-                        //u_idx[v]++;
-                        // flag start or stop extrude
-                    //}
-                    
+                raycaster.set(origin, direction);
+                const intersects = raycaster.intersectObject(geo_obj);
+                if(intersects.length > 0){
+                    pts.push(intersects[0].point);
+                    if(intersects.length > 1) console.log('two hits!!!');
                 }
-                if(ty < bb1.min.y) break;
-                layer_count++;
+                y -= layer_height / rot_res;
+                if(y < bb1.min.y) break;
+                origin.applyMatrix4(rotation);
+                direction.set(0,y,0).sub(origin);
             }
+
             const curve = new CatmullRomCurve3(pts);
-            curve.arcLengthDivisions = 800;
+            curve.arcLengthDivisions = 500;
             
             var code = '';
             // pts = curve.getPoints(Math.round(curve.getLength()*this.code_res));
@@ -74,6 +90,27 @@ export const vase = {
         } 
     }, 
 };
+
+
+            // var layer_count = 0;
+            // var ty = start_y;
+            // for(let i=0; i<10000; i++){
+            //     for(let v=0; v<1/res_v; v++){
+            //         ty = start_y - (layer_count * this.layer_height + this.layer_height * v*res_v);
+            //         //var v2 = tests[v].slice(u_idx[v], u_idx[v]+50).sort((a,b)=> Math.abs(a.y-ty)-Math.abs(b.y-ty))[0]; // Don't need to sort this ?!?!?!?!?! instead, just search down u from last time at this v
+            //         //var v2 = tests[v][u_idx[v]];
+            //         var pt = tests[v].sort((a,b)=> Math.abs(a.y-ty)-Math.abs(b.y-ty))[0];
+            //         //if(Math.abs(pt.y-ty) < this.layer_height/2){
+            //             pts.push(pt.clone());
+            //             //u_idx[v]++;
+            //             // flag start or stop extrude
+            //         //}
+                    
+            //     }
+            //     if(ty < bb1.min.y) break;
+            //     layer_count++;
+            // }
+
 
 // for(let u=0; u<1; u+=this.res){
 //     for(let v=0; v<1; v+=this.res){
