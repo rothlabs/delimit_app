4d0f33a12daf5a15310804f2508fdb5ff4055dd8
Julian Erickson <julian@rothlabs.com>
2023-08-06 00:24:02 +0000

    save before vase4

--- web/core/static/core/store/reckon/vase.js
+++ web/core/static/core/store/reckon/vase.js
@@ -23,35 +23,36 @@ for(let v=0; v<1/res_v; v++){
     }
 }
 
-//const origin = new Vector3();
-//const direction = new Vector3();
-//const raycaster = new Raycaster();
-//const material = new MeshBasicMaterial();
+const origin = new Vector3();
+const direction = new Vector3();
+const raycaster = new Raycaster();
+const material = new MeshBasicMaterial();
 
-//const geo_res = 60;
-const rot_res = 60;
-const rot_step = Math.PI*2/rot_res;
-const target_angles = [];
-for(let i=0; i<rot_res; i++){
-    target_angles.push(rot_step*i);
-}
-for(let i=rot_res; i>0; i--){
-    target_angles.push(rot_step*i);
-}
-console.log('target_angles');
-console.log(target_angles);
-//const rotation = new Matrix4().makeRotationY(Math.PI*2/rot_res);
+const geo_res = 50;
+const rot_res = 45;
+const rot_step = (Math.PI*2/rot_res);
+// const rot_step = Math.PI*2/rot_res;
+// const target_angles = [];
+// for(let i=0; i<rot_res; i++){
+//     target_angles.push(rot_step*i);
+// }
+// for(let i=rot_res; i>0; i--){
+//     target_angles.push(rot_step*i);
+// }
+// console.log('target_angles');
+// console.log(target_angles);
+const m1 = new Matrix4();//.makeRotationY(Math.PI*2/rot_res);
 
-const local_res = 0.005;
-const local_size = 40;
-const local_offset = local_size*local_res/2;
-const local_points = [];
-for(let u=0; u<local_size; u++){
-    local_points[u] = [];
-    for(let v=0; v<local_size; v++){
-        local_points[u][v] = {p: new Vector3(10000,10000,10000), u:0, v:0};
-    }
-}
+// const local_res = 0.005;
+// const local_size = 40;
+// const local_offset = local_size*local_res/2;
+// const local_points = [];
+// for(let u=0; u<local_size; u++){
+//     local_points[u] = [];
+//     for(let v=0; v<local_size; v++){
+//         local_points[u][v] = {p: new Vector3(10000,10000,10000), u:0, v:0};
+//     }
+// }
 
 const layer_height = 6;
 
@@ -77,45 +78,35 @@ export const vase = {
             }
 
 
-            // const geo_obj = new Mesh(
-            //     new ParametricGeometry(surface.get_point, geo_res, geo_res),
-            //     material
-            // );
+            const geo_obj = new Mesh(
+                new ParametricGeometry(surface.get_point, geo_res, geo_res),
+                material
+            );
             var pts = [];
-            var u = 1;
-            var v = 0;
-            surface.get_point(u, v, v1);
+            //var u = 1;
+            //var v = 0;
+            surface.get_point(1, 0, v1);
             var y = v1.y;
+            origin.set(0,y,1000);
+            direction.set(0,y,0).sub(origin);
             axis.set(0,y,0);
-            for(let i=0; i<10000; i++){
+            for(let i=0; i<10000; i++){ 
                 v_sum.set(0,0,0);
-                target_angles.forEach(target_angle => {
-                    for(let uu=0; uu<local_size; uu++){
-                        for(let vv=0; vv<local_size; vv++){ 
-                            local_points[uu][vv].u = u-local_offset+(uu*local_res);
-                            local_points[uu][vv].v = v-local_offset+(vv*local_res);
-                            if(local_points[uu][vv].u >= 0 && local_points[uu][vv].u <= 1){
-                                if(local_points[uu][vv].v >= 0 && local_points[uu][vv].v <= 1){
-                                    surface.get_point(local_points[uu][vv].u, local_points[uu][vv].v, local_points[uu][vv].p);
-                                    v_sum.add(local_points[uu][vv].p);
-                                }
-                            }
-                        }
+                for(let k=0; k<rot_res; k++){ 
+                    raycaster.set(origin, direction);
+                    const intersects = raycaster.intersectObject(geo_obj);
+                    if(intersects.length > 0){
+                        pts.push(intersects[0].point);
+                        v_sum.add(intersects[0].point);
+                        if(intersects.length > 1) console.log('two hits!!!');
                     }
-                    var best_fit = local_points.flat().sort((a,b)=>{
-                        v1.set(axis.x, a.p.y, axis.z);
-                        v2.set(axis.x, b.p.y, axis.z);
-                        var angle_a = z_dir.angleTo(v3.copy(a.p).sub(v1)); 
-                        var angle_b = z_dir.angleTo(v3.copy(b.p).sub(v2)); 
-                        return (Math.abs(a.p.y-y)+Math.abs(angle_a-target_angle)) - (Math.abs(b.p.y-y)+Math.abs(angle_b-target_angle));
-                    })[0];
-                    pts.push(best_fit.p.clone());
-                    u = best_fit.u;
-                    v = best_fit.v; 
-                    y -= layer_height / target_angles.length;
-                });
-                if(y < bb1.min.y) break;
-                v_sum.divideScalar(target_angles.length);
+                    y -= layer_height / rot_res;
+                    if(y < bb1.min.y) break;
+                    origin.set(axis.x, y, axis.z+1000);
+                    origin.applyMatrix4(m1.makeRotationY(rot_step*k));
+                    direction.set(axis.x,y,axis.z).sub(origin);
+                }
+                v_sum.divideScalar(rot_res);
                 axis.copy(v_sum);
             }
 
@@ -209,3 +200,44 @@ export const vase = {
 //     }
 // }
 
+
+
+
+// var pts = [];
+//             //var u = 1;
+//             //var v = 0;
+//             surface.get_point(u, v, v1);
+//             var y = v1.y;
+//             axis.set(0,y,0);
+//             for(let i=0; i<10000; i++){ 
+//                 v_sum.set(0,0,0);
+//                 target_angles.forEach(target_angle => {
+//                     for(let uu=0; uu<local_size; uu++){
+//                         for(let vv=0; vv<local_size; vv++){ 
+//                             local_points[uu][vv].u = u-local_offset+(uu*local_res);
+//                             local_points[uu][vv].v = v-local_offset+(vv*local_res);
+//                             if(local_points[uu][vv].u >= 0 && local_points[uu][vv].u <= 1){
+//                                 if(local_points[uu][vv].v >= 0 && local_points[uu][vv].v <= 1){
+//                                     surface.get_point(local_points[uu][vv].u, local_points[uu][vv].v, local_points[uu][vv].p);
+                                    
+//                                 }
+//                             }
+//                         }
+//                     }
+//                     var best_fit = local_points.flat().sort((a,b)=>{
+//                         v1.set(axis.x, a.p.y, axis.z);
+//                         v2.set(axis.x, b.p.y, axis.z);
+//                         var angle_a = z_dir.angleTo(v3.copy(a.p).sub(v1)); 
+//                         var angle_b = z_dir.angleTo(v3.copy(b.p).sub(v2)); 
+//                         return (Math.abs(a.p.y-y)+Math.abs(angle_a-target_angle)) - (Math.abs(b.p.y-y)+Math.abs(angle_b-target_angle));
+//                     })[0];
+//                     pts.push(best_fit.p.clone());
+//                     v_sum.add(local_points[uu][vv].p);
+//                     u = best_fit.u;
+//                     v = best_fit.v; 
+//                     y -= layer_height / target_angles.length;
+//                 });
+//                 if(y < bb1.min.y) break;
+//                 v_sum.divideScalar(target_angles.length);
+//                 axis.copy(v_sum);
+//             }
