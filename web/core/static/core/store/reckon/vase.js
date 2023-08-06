cc13a2bd414453a0af15f078b52326ffc908449c
Julian Erickson <julian@rothlabs.com>
2023-08-06 02:21:20 +0000

    vase works

--- web/core/static/core/store/reckon/vase.js
+++ web/core/static/core/store/reckon/vase.js
@@ -1,5 +1,5 @@
 import {current} from 'immer';
-import {Vector3, Matrix4, MathUtils, CatmullRomCurve3, Box3, Raycaster, Mesh, MeshBasicMaterial} from 'three';
+import {Vector3, Matrix4, MathUtils, CatmullRomCurve3, Box3, Raycaster, Mesh, MeshBasicMaterial, Line3} from 'three';
 //import {NURBSCurve} from 'three/examples/jsm/curves/NURBSCurve';
 import {ParametricGeometry} from 'three/examples/jsm/geometries/ParametricGeometry';
 
@@ -9,9 +9,11 @@ const v3 = new Vector3();
 const z_dir = new Vector3(0,0,1);
 const axis = new Vector3();
 const v_sum = new Vector3();
+
+const l1 = new Line3();
 //const v_count = 0;
 
-const res_v = .005;
+const res_v = .001;
 const res_u = .001;
 const tests = [];
 //const u_idx = [];
@@ -19,19 +21,18 @@ for(let v=0; v<1/res_v; v++){
     tests[v] = [];
     //u_idx.push(0);
     for(let u=0; u<1/res_u; u++){
-        tests[v][u] = new Vector3();
+        tests[v][u] = {p: new Vector3(), n: new Vector3()};
     }
 }
 
 const origin = new Vector3();
-const direction = new Vector3();
+const destination = new Vector3();
 const raycaster = new Raycaster();
 const material = new MeshBasicMaterial();
 
 const geo_res = 50;
-const rot_res = 45;
-const rot_step = (Math.PI*2/rot_res);
-// const rot_step = Math.PI*2/rot_res;
+const rot_res = 30;
+const rot_step = Math.PI*2/rot_res;
 // const target_angles = [];
 // for(let i=0; i<rot_res; i++){
 //     target_angles.push(rot_step*i);
@@ -54,14 +55,17 @@ const m1 = new Matrix4();//.makeRotationY(Math.PI*2/rot_res);
 //     }
 // }
 
-const layer_height = 6;
+
+
+const layer_height = 1;
 
 export const vase = {
     
     code_res: .2, // code_res = arc length between G1 if constant curve like an arc
     node(d, n){
         try{
-            if(d.studio.mode == 'graph') return;
+            console.log('compute vase');
+            //if(d.studio.mode == 'graph') return;
             delete d.n[n].c.g_code;
             delete d.n[n].c.curve;
             delete d.n[n].ax.curve;
@@ -72,46 +76,83 @@ export const vase = {
             for(let v=0; v<1/res_v; v++){ 
                 //u_idx[v] = 0;
                 for(let u=0; u<1/res_u; u++){
-                    surface.get_point(1-u*res_u, v*res_v, tests[v][u]);
-                    bb1.expandByPoint(tests[v][u]);
+                    var uu = 1-u*res_u;
+                    var vv = v*res_v;
+                    surface.get_point(uu,      vv,      tests[v][u].p);
+                    //surface.get_point(uu+.005, vv,      v1);
+                    //surface.get_point(uu,      vv+.005, v2);
+                    //tests[v][u].n.copy(tests[v][u].p).add(v1.sub(tests[v][u].p).cross(v2.sub(tests[v][u].p)).normalize());
+                    bb1.expandByPoint(tests[v][u].p);
+                    //surface.get_point(1-u*res_u, v*res_v, tests[v][u].p);
+                    
                 }
             }
+            const test_points = tests.flat();//.sort((a,b)=> a.y-b.y);
+            const tpl = {};
+            // for(let i=-1000; i<1000; i++){
+            //     tpl.push([]);
+            // }
+            for(let i=0; i<test_points.length; i++){
+                var idx = Math.round(test_points[i].p.y);
+                if(tpl['l'+idx] == undefined) tpl['l'+idx] = [];
+                tpl['l'+idx].push(test_points[i]);
+            }
+            //console.log(tpl);
 
 
-            const geo_obj = new Mesh(
-                new ParametricGeometry(surface.get_point, geo_res, geo_res),
-                material
-            );
+            // const geo_obj = new Mesh(
+            //     new ParametricGeometry(surface.get_point, geo_res, geo_res),
+            //     material
+            // );
+            console.log('compute vase phase 2');
             var pts = [];
             //var u = 1;
             //var v = 0;
             surface.get_point(1, 0, v1);
             var y = v1.y;
             origin.set(0,y,1000);
-            direction.set(0,y,0).sub(origin);
+            destination.set(0,y,0);//.sub(origin);
             axis.set(0,y,0);
             for(let i=0; i<10000; i++){ 
                 v_sum.set(0,0,0);
                 for(let k=0; k<rot_res; k++){ 
-                    raycaster.set(origin, direction);
-                    const intersects = raycaster.intersectObject(geo_obj);
-                    if(intersects.length > 0){
-                        pts.push(intersects[0].point);
-                        v_sum.add(intersects[0].point);
-                        if(intersects.length > 1) console.log('two hits!!!');
-                    }
+                    l1.set(origin, destination);
+                    const ry = Math.round(y);
+                    var tp = [];
+                    if(tpl['l'+ry]) tp = tpl['l'+ry];
+                    //for(let idx=ry-4; idx<=ry+4; idx++){
+                    //    if(tpl['l'+idx]) tp = tp.concat(tpl['l'+idx]);
+                    //}
+                    //console.log(tp);
+                    if(tp.length < 1) break;
+
+                    var pt = tp.sort((a,b)=>{
+                        //if(a.n.dot(l1.delta(v3)) > 0) l1.closestPointToPoint(a.p, false, v1)
+                        //else return 1;//v1.set(10000,10000,10000);
+                        //if(b.n.dot(l1.delta(v3)) > 0) l1.closestPointToPoint(b.p, false, v2);
+                        //else return -1;//v2.set(10000,10000,10000);
+                        l1.closestPointToPoint(a.p, true, v1);
+                        l1.closestPointToPoint(b.p, true, v2);
+                        return v1.distanceTo(a.p)-v2.distanceTo(b.p);
+                    })[0];
+                    //console.log(pt);
+                    
+                    pts.push(pt.p);
+                    v_sum.add(pt.p);
                     y -= layer_height / rot_res;
-                    if(y < bb1.min.y) break;
+                    //if(y < bb1.min.y) break;
                     origin.set(axis.x, y, axis.z+1000);
                     origin.applyMatrix4(m1.makeRotationY(rot_step*k));
-                    direction.set(axis.x,y,axis.z).sub(origin);
+                    destination.set(axis.x,y,axis.z);//.sub(origin);
                 }
                 v_sum.divideScalar(rot_res);
                 axis.copy(v_sum);
             }
 
+            console.log('compute vase phase 3');
+
             const curve = new CatmullRomCurve3(pts);
-            curve.arcLengthDivisions = 500;
+            curve.arcLengthDivisions = 2000;
             
             var code = '';
             // pts = curve.getPoints(Math.round(curve.getLength()*this.code_res));
@@ -128,6 +169,35 @@ export const vase = {
         } 
     }, 
 };
+
+
+// var pts = [];
+//             //var u = 1;
+//             //var v = 0;
+//             surface.get_point(1, 0, v1);
+//             var y = v1.y;
+//             origin.set(0,y,1000);
+//             direction.set(0,y,0).sub(origin);
+//             axis.set(0,y,0);
+//             for(let i=0; i<10000; i++){ 
+//                 v_sum.set(0,0,0);
+//                 for(let k=0; k<rot_res; k++){ 
+//                     raycaster.set(origin, direction);
+//                     const intersects = raycaster.intersectObject(geo_obj);
+//                     if(intersects.length > 0){
+//                         pts.push(intersects[0].point);
+//                         v_sum.add(intersects[0].point);
+//                         if(intersects.length > 1) console.log('two hits!!!');
+//                     }
+//                     y -= layer_height / rot_res;
+//                     if(y < bb1.min.y) break;
+//                     origin.set(axis.x, y, axis.z+1000);
+//                     origin.applyMatrix4(m1.makeRotationY(rot_step*k));
+//                     direction.set(axis.x,y,axis.z).sub(origin);
+//                 }
+//                 v_sum.divideScalar(rot_res);
+//                 axis.copy(v_sum);
+//             }
 
 
 // const geo_obj = new Mesh(
